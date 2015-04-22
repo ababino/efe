d3afb4946e973cc774d425e5bc7b2225ba033512
andres <ababino@gmail.com>
2015-04-22 16:50:38 -0300

    add soution to item c

--- ej15.py
+++ ej15.py
@@ -38,6 +38,25 @@ def exp1():
 	plt.show()
 
 
+def exp2():
+	I = 15.
+	delta_t = 1.
+	n = 1000
+	dt = delta_t / n
+	p = I*dt
+	hist_data = [binomial_sample(n, p) for x in xrange(1000)]
+	theory_x = range(0, n + 1)
+	theory_y = [binomial(x, n, p) for x in theory_x]
+	plt.hist(hist_data, bins=np.arange(0.5, 1000.5), normed=1, label=r'$Simulaci\'on$')
+	plt.plot(theory_x, theory_y, 'k--*', label=r'$Distribuci\'on\ Te\'orica$')
+	plt.xlabel(r'$N\'umero\ de\ Fotones\ Emitidos$')
+	plt.ylabel(r'$Tasa$')
+	plt.legend()
+	plt.xlim([0, 35])
+	plt.savefig('figc.jpg')
+	plt.show()
+
+
 def main():
 	n = 10
 	p = 0.7
@@ -48,7 +67,8 @@ def main():
 	print 'n = %s, p = %s, s= %s' % (n, p, s)
 	print '-----b)-------'
 	exp1()
-
+	print '-----c)-------'
+	exp2()
 
 
 if __name__ == '__main__':
