ecabccdb06cf2f270d2cc266aa24da159f279dc6
Andres Babino <ababino@gmail.com>
2015-07-21 15:45:02 -0300

    fix sigma a23 error

--- cuarta/g8e13.py
+++ cuarta/g8e13.py
@@ -5,7 +5,7 @@ import numpy as np
 import seaborn as sns
 import argparse
 from scipy import optimize
-#sns.set_style("dark")
+
 
 def load_data():
     file_data = open('doble_exp.dat', 'r')
@@ -26,27 +26,27 @@ def load_data():
 
 def fit_fun(tita, xdata):
     yfit = tita[0]
-    yfit += tita[1] * np.exp(-xdata/tita[3])
-    yfit += tita[2] * np.exp(-xdata/tita[4])
+    yfit += tita[1] * np.exp(-xdata / tita[3])
+    yfit += tita[2] * np.exp(-xdata / tita[4])
     return yfit
 
 
 def fit_fun_grad(tita, xdata):
     yfitd = np.ones((len(tita), len(xdata)))
-    yfitd[1, :] = np.exp(-xdata/tita[3])
-    yfitd[2, :] = np.exp(-xdata/tita[4])
-    yfitd[3, :] = tita[1] * np.exp(-xdata/tita[3]) * xdata / tita[3]**2
-    yfitd[4, :] = tita[2] * np.exp(-xdata/tita[4]) * xdata / tita[4]**2
+    yfitd[1, :] = np.exp(-xdata / tita[3])
+    yfitd[2, :] = np.exp(-xdata / tita[4])
+    yfitd[3, :] = tita[1] * np.exp(-xdata / tita[3]) * xdata / tita[3]**2
+    yfitd[4, :] = tita[2] * np.exp(-xdata / tita[4]) * xdata / tita[4]**2
     return yfitd
 
 
 def fit_fun_hess(tita, xdata):
     yfitd = fit_fun_grad(tita, xdata)
     yfith = np.zeros((len(tita), len(tita), len(xdata)))
-    yfith[1, 3, :] = yfitd[1,:] * xdata/tita[3]**2
-    yfith[2, 4, :] = yfitd[2,:] * xdata/tita[4]**2
-    yfith[3, 3, :] = yfitd[3,:] * (xdata / tita[3] - 2) / tita[3]
-    yfith[4, 4, :] = yfitd[4,:] * (xdata / tita[4] - 2) / tita[4]
+    yfith[1, 3, :] = yfitd[1, :] * xdata / tita[3]**2
+    yfith[2, 4, :] = yfitd[2, :] * xdata / tita[4]**2
+    yfith[3, 3, :] = yfitd[3, :] * (xdata / tita[3] - 2) / tita[3]
+    yfith[4, 4, :] = yfitd[4, :] * (xdata / tita[4] - 2) / tita[4]
     yfith[3, 1, :] = yfith[1, 3, :]
     yfith[4, 2, :] = yfith[2, 4, :]
     return yfith
@@ -54,8 +54,8 @@ def fit_fun_hess(tita, xdata):
 
 def plot_fit(tita, xdata, ydata, yerr):
     f = plt.figure(1)
-    ax = f.add_subplot(1,1,1)
-    ax.errorbar(xdata, ydata, yerr = yerr, fmt='.', label='Datos')
+    ax = f.add_subplot(1, 1, 1)
+    ax.errorbar(xdata, ydata, yerr=yerr, fmt='.', label='Datos')
     ax.plot(xdata, fit_fun(tita, xdata), label='Ajuste, 3 parametros')
     ax.set_yscale('log')
     plt.xlabel('Tiempo (s)')
@@ -68,27 +68,31 @@ def chi2(tita, xdata, ydata):
     yfit = fit_fun(tita, xdata)
     chi2 = sum((ydata - yfit)**2 / ydata)
     return chi2
+
+
 def jac(tita, xdata, ydata):
     yfit = fit_fun(tita, xdata)
     yfitd = fit_fun_grad(tita, xdata)
     a = (yfit - ydata) / ydata
     j = 2 * np.dot(yfitd, a.T)
     return j
+
+
 def hessian(tita, xdata, ydata):
     yfit = fit_fun(tita, xdata)
     yfitd = fit_fun_grad(tita, xdata)
     yfith = fit_fun_hess(tita, xdata)
     a = (yfit - ydata) / ydata
     a = np.tile(np.tile(a, (len(tita), 1)), (len(tita), 1, 1))
-    h = 2 *  np.sum(yfith * a, axis=2)
-    h += 2 *sum([np.outer(c, c) / e for c, e in zip(yfitd.T, ydata)])
+    h = 2 * np.sum(yfith * a, axis=2)
+    h += 2 * sum([np.outer(c, c) / e for c, e in zip(yfitd.T, ydata)])
     return h
 
 
 def e13a(time, count, a4, a5):
     A = np.ones((len(time), 3))
-    A[:,1] = np.exp(-time / a4)
-    A[:,2] = np.exp(-time / a5)
+    A[:, 1] = np.exp(-time / a4)
+    A[:, 2] = np.exp(-time / a5)
     V = np.eye(len(time))
     np.fill_diagonal(V, count)
     av = np.dot(A.T, np.linalg.inv(V))
@@ -102,60 +106,57 @@ def e13a(time, count, a4, a5):
 def e13b(time, count, x0=None):
     if x0 is None:
         x0 = [10.6888, 127.9398, 960.8654, 200., 34.]
-    objfun = lambda x: chi2(x, time, count)
-    objfunjac = lambda x: jac(x, time, count)
-    objfunhess = lambda x: hessian(x, time, count)
-    #'Newton-CG' 'Nelder-Mead' 'BFGS' 'Powell' 'CG'
-    res = optimize.minimize(objfun, x0, method='BFGS', jac=objfunjac,
-                               hess=None,
-                               options={'disp': True})
-    print(0.5*res.hess_inv)
-    return res, objfunhess(res.x)
+    res = optimize.minimize(lambda x: chi2(x, time, count), x0, method='BFGS',
+                            jac=lambda x: hessian(x, time, count), hess=None,
+                            options={'disp': True})
+    print(0.5 * res.hess_inv)
+    return res, hessian(res.x, time, count)
 
 
-def e13c(time, count, x0=None, cv=1, row_int=(-3,3), col_int=(-35, 45),
+def e13c(time, count, x0=None, cv=1, row_int=(-3, 3), col_int=(-35, 45),
          plot_soloid=True):
     if x0 is None:
         x0 = [10.6888, 127.9398, 960.8654, 200., 34.]
-    objfun = lambda x: chi2(x, time, count)
-    objfunjac = lambda x: jac(x, time, count)
-    res = optimize.minimize(objfun, x0, method='BFGS', jac=objfunjac)
-    row = res.x[4] + np.linspace(row_int[0],row_int[1],50)
-    col = res.x[3] + np.linspace(col_int[0],col_int[1],50)
-    X,Y = np.meshgrid(row,col)
+    res = optimize.minimize(lambda x: chi2(x, time, count), x0, method='BFGS',
+                            jac=lambda x: jac(x, time, count))
+    row = res.x[4] + np.linspace(row_int[0], row_int[1], 50)
+    col = res.x[3] + np.linspace(col_int[0], col_int[1], 50)
+    X, Y = np.meshgrid(row, col)
     if plot_soloid:
-        f = np.vectorize(lambda x, y: objfun([res.x[0], res.x[1], res.x[2], x, y]))
+        a = res.x
+        f = np.vectorize(lambda x: chi2([a[0], a[1], a[2], x, y], time, count))
         Z = f(Y, X)
         c1 = sns.color_palette()[0]
-        plt.contour(X, Y, Z, [objfun(res.x)+cv], colors=[c1])
+        plt.contour(X, Y, Z, [objfun(res.x) + cv], colors=[c1])
     x0 = res.x[:3]
+
     @np.vectorize
     def f(x, y):
         tita, ava = e13a(time, count, x, y)
         params = np.concatenate([tita, [x, y]])
         return objfun(params)
     Z = f(Y, X)
-    ct = plt.contour(X, Y, -Z, [-objfun(res.x)-cv], colors=[sns.color_palette()[1]])
+    ct = plt.contour(X, Y, -Z, [-objfun(res.x) - cv],
+                     colors=[sns.color_palette()[1]])
     plt.plot([res.x[4]], [res.x[3]], 'o')
     plt.xlabel('$a_5$')
     plt.ylabel('$a_4$')
     p = ct.collections[0].get_paths()[0]
     v = p.vertices
-    x = v[:,0]
-    y = v[:,1]
+    x = v[:, 0]
+    y = v[:, 1]
     print(min(x), max(x), min(y), max(y))
     return res
 
 
-
 def main(args):
     time, count, poi_err = load_data()
-    a4 =209.69
-    a5 =34.244
+    a4 = 209.69
+    a5 = 34.244
     if '13a' in args.items:
         tita, ava = e13a(time, count, a4, a5)
         a23 = tita[1] / tita[2]
-        jacob = np.array([1 / tita[1], -a23 / tita[2]])
+        jacob = np.array([1 / tita[2], -a23 / tita[2]])
         a23var = np.dot(np.dot(jacob, ava[1:, 1:]), jacob.T)
         print('Guia 6, ejercicio 13, item a')
         print('tita = ' + str(tita))
@@ -175,7 +176,7 @@ def main(args):
         print('ch2 = ' + str(chi2(res.x, time, count)))
         print('d.f. = ' + str(len(time) - 5))
         np.set_printoptions(precision=2)
-        cov = np.linalg.inv(0.5* H)
+        cov = np.linalg.inv(0.5 * H)
         print(cov)
         print(np.sqrt(cov.diagonal()))
     if '13c' in args.items:
@@ -183,12 +184,14 @@ def main(args):
         plt.savefig('fig1.jpg')
         plt.show()
     if '13d' in args.items:
-        e13c(time, count, cv=5.86, row_int=(-7,7), col_int=(-70, 140), plot_soloid=False)
+        e13c(time, count, cv=5.86, row_int=(-7, 7), col_int=(-70, 140),
+             plot_soloid=False)
         plt.savefig('fig11.jpg')
         plt.show()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Resuleve el ejercicio 13 de la guia 8.')
+    parser = argparse.ArgumentParser(
+        description='Resuleve el ejercicio 13 de la guia 8.')
     parser.add_argument('items', metavar='I', type=str, nargs='+',
                         help='Los items a resolver.')
     args = parser.parse_args()
--- cuarta/g9.py
+++ cuarta/g9.py
@@ -19,37 +19,38 @@ from scipy import optimize
 
 
 def my_hist(data, bins, err_type='poisson', **kwargs):
-	"""Histogram with poissonian error bars."""
-	y, bin_edges = np.histogram(data, bins=bins)
-	normalization = sum(y*np.diff(bin_edges))
-	if err_type == 'poisson':
-		yerr = np.sqrt(y) / normalization
-	elif err_type == 'binomial':
-		yerr = np.sqrt(y * ( 1 - y / normalization) / normalization)
-	y = y.astype(np.float) / normalization
-	ax = plt.bar(bin_edges[:-1], y, yerr=yerr, width=1.0, ecolor='r', **kwargs)
-	return ax
+    """Histogram with poissonian error bars."""
+    y, bin_edges = np.histogram(data, bins=bins)
+    normalization = sum(y * np.diff(bin_edges))
+    if err_type == 'poisson':
+        yerr = np.sqrt(y) / normalization
+    elif err_type == 'binomial':
+        yerr = np.sqrt(y * (1 - y / normalization) / normalization)
+    y = y.astype(np.float) / normalization
+    ax = plt.bar(bin_edges[:-1], y, yerr=yerr, width=1.0, ecolor='r', **kwargs)
+    return ax
 
 
 def cramer_von_mises(data):
     ecdf = ECDF(data)
-    fun= lambda x: (ecdf(x) - norm.cdf(x, 0, 2.5))**2
+
+    def cvm_integrand(x):
+        return (ecdf(x) - norm.cdf(x, 0, 2.5))**2
     data[-1] = np.inf
     data[len(data)] = np.inf
     jumps = sorted(data.unique())
     cvm = 0
-    cvm = integrate.quad(fun, -np.inf, min(data))[0]
-    #cvm += integrate.quad(fun, min(data), max(data))[0]
+    cvm = integrate.quad(cvm_integrand, -np.inf, min(data))[0]
     cvm += integrate.quad(fun, max(data), np.inf)[0]
     for a, b in zip(jumps[:-1], jumps[1:]):
-        cvm += integrate.quad(fun, a, b)[0]
+        cvm += integrate.quad(cvm_integrand, a, b)[0]
     return cvm
 
 
 def kolmolgorov(data):
     ecdf = ECDF(data)
     d1 = abs(ecdf(data) - norm.cdf(data, 0, 2.5))
-    d2 = abs(ecdf(data) - norm.cdf(data, 0, 2.5) - 1/len(data))
+    d2 = abs(ecdf(data) - norm.cdf(data, 0, 2.5) - 1 / len(data))
     i1 = abs(d1).argmax()
     i2 = abs(d2).argmax()
     d = max(d1[i1], d2[i2])
@@ -60,24 +61,24 @@ def chi2_from_sample(data):
     histdata = np.histogram(data, 56, range=(-7, 7))
     N = sum(histdata[0])
     y_data = histdata[0]
-    yerr2 = y_data * ( 1 - y_data/ N)
-    x = histdata[1][:-1] + np.diff(histdata[1])/2
+    yerr2 = y_data * (1 - y_data / N)
+    x = histdata[1][:-1] + np.diff(histdata[1]) / 2
     norm025 = norm(0, 2.5)
-    y_fit = N*(norm025.cdf(histdata[1][1:]) - norm025.cdf(histdata[1][:-1]))
+    y_fit = N * (norm025.cdf(histdata[1][1:]) - norm025.cdf(histdata[1][:-1]))
     chi2_m = sum((y_data - y_fit)**2 / yerr2)
     return chi2_m, chi2.sf(chi2_m, len(x)), len(x)
 
 
 def plot_hist(data, nbins=56):
-    ax = sns.distplot(data, nbins, kde=False, hist_kws={'range':(-7, 7),
-                                                     'label':'Datos'})
+    ax = sns.distplot(data, nbins, kde=False,
+                      hist_kws={'range': (-7, 7), 'label': 'Datos'})
     histdata = np.histogram(data, nbins, range=(-7, 7))
-    x = histdata[1][:-1] + np.diff(histdata[1])/2
-    yerr = np.sqrt(histdata[0] * (1- histdata[0]/sum(histdata[0])))
+    x = histdata[1][:-1] + np.diff(histdata[1]) / 2
+    yerr = np.sqrt(histdata[0] * (1 - histdata[0] / sum(histdata[0])))
     ax.errorbar(x, histdata[0], yerr=yerr, fmt='.')
     N = sum(histdata[0])
     norm025 = norm(0, 2.5)
-    y_fit = N*(norm025.cdf(histdata[1][1:]) - norm025.cdf(histdata[1][:-1]))
+    y_fit = N * (norm025.cdf(histdata[1][1:]) - norm025.cdf(histdata[1][:-1]))
     ax.plot(x, y_fit, label='Nº de Eventos Esperados')
     return ax
 
@@ -86,17 +87,29 @@ def ej13c(cv, data):
     dfs = []
     for N in cv.index[:-2]:
         T, chi2pval, df = chi2_from_sample(data[:N])
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [T], 'Test': ['Chi2'], 'Estadistico': ['Medido']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [T],
+                                 'Test': ['$\chi^2$'],
+                                 'Estadistico': ['Medido']}))
         Tc = chi2.isf(0.01, df)
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc], 'Test': ['Chi2'], 'Estadistico': ['Critico']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc],
+                                 'Test': ['$\chi^2$'],
+                                 'Estadistico': ['Critico']}))
         T = kstest(data[:N], norm(0, 2.5).cdf)[0]
         Tc = cv.loc[N, 'T_k^{critico}']
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [T], 'Test': ['Kolmolgorov'], 'Estadistico':['Medido']}))
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc], 'Test': ['Kolmolgorov'], 'Estadistico': ['Critico']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [T],
+                                 'Test': ['Kolmolgorov'],
+                                 'Estadistico': ['Medido']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc],
+                                 'Test': ['Kolmolgorov'],
+                                 'Estadistico':  ['Critico']}))
         T = cramer_von_mises(data[:N])
         Tc = cv.loc[N, 'T_c^{critico}']
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T':[T], 'Test': ['Cramer Von-Mises'], 'Estadistico':['Medido']}))
-        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc], 'Test': ['Cramer Von-Mises'], 'Estadistico': ['Critico']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [T],
+                                 'Test': ['Cramer Von-Mises'],
+                                 'Estadistico': ['Medido']}))
+        dfs.append(pd.DataFrame({'Eventos': [N], 'T': [Tc],
+                                 'Test': ['Cramer Von-Mises'],
+                                 'Estadistico': ['Critico']}))
     df = pd.concat(dfs)
     g = sns.FacetGrid(df, col='Test', hue='Estadistico', sharey=False)
     g.map(plt.scatter, 'Eventos', 'T')
@@ -104,28 +117,28 @@ def ej13c(cv, data):
     g.add_legend()
     g.set(xticks=[0, 4000, 8000])
     plt.savefig('fig3.jpg')
-    print(df[(df['Estadistico']=='Medido') & (df['Test']=='Cramer Von-Mises')])
     plt.show()
+
 
 def ej14(data_x, data_y, s):
     a2, a1 = np.polyfit(data_x, data_y, 1)
     chi2_t_list = []
     chi2_f_list = []
     for _ in range(1000):
-    	new_y = np.random.normal(a1 + a2*data_x, s)
+        new_y = np.random.normal(a1 + a2 * data_x, s)
         p = np.polyfit(data_x, new_y, 1)
-        chi2_t_list.append(sum(((new_y -a1 - a2*data_x)/s)**2))
-        chi2_f_list.append(sum(((new_y -p[1] - p[0]*data_x)/s)**2))
+        chi2_t_list.append(sum(((new_y - a1 - a2 * data_x) / s)**2))
+        chi2_f_list.append(sum(((new_y - p[1] - p[0] * data_x) / s)**2))
     plt.figure(1)
-    my_hist(chi2_t_list,20, label='Datos')
-    x = np.linspace(0, 30,100)
+    my_hist(chi2_t_list, 20, label='Datos')
+    x = np.linspace(0, 30, 100)
     chi211 = chi2.pdf(x, 11)
     plt.plot(x, chi211, label='$\chi^2_{11}$', color=sns.color_palette()[2])
     plt.legend()
     plt.savefig('fig4.jpg')
     plt.figure(2)
-    my_hist(chi2_f_list,20, label='Datos')
-    x = np.linspace(0, 30,100)
+    my_hist(chi2_f_list, 20, label='Datos')
+    x = np.linspace(0, 30, 100)
     plt.plot(x, chi211, label='$\chi^2_{11}$', color=sns.color_palette()[2])
     chi29 = chi2.pdf(x, 9)
     plt.plot(x, chi29, label='$\chi^2_{9}$', color=sns.color_palette()[3])
@@ -133,9 +146,10 @@ def ej14(data_x, data_y, s):
     plt.savefig('fig5.jpg')
     plt.show()
 
+
 def main(args):
     if '13' in ''.join(args.items):
-        data = pd.read_csv('datos-G9E13.dat',squeeze=True, header=None,
+        data = pd.read_csv('datos-G9E13.dat', squeeze=True, header=None,
                            names=['Data'])
         cv = pd.read_csv('tabla-cvm.txt', sep='\t', skiprows=1, index_col='N')
     if '13bi' in args.items:
@@ -158,13 +172,16 @@ def main(args):
         print('--------13 Item c-------')
         ej13c(cv, data)
     if '14a' in args.items:
-    	x = np.array([2.00, 2.10, 2.20, 2.30, 2.40, 2.50, 2.60, 2.70, 2.80, 2.90, 3.00])
-    	y = np.array([2.78, 3.29, 3.29, 3.33, 3.23, 3.69, 3.46, 3.87, 3.62, 3.40, 3.99])
-    	s = 0.3
+        x = np.array([2.00, 2.10, 2.20, 2.30, 2.40, 2.50, 2.60, 2.70, 2.80,
+                      2.90, 3.00])
+        y = np.array([2.78, 3.29, 3.29, 3.33, 3.23, 3.69, 3.46, 3.87, 3.62,
+                      3.40, 3.99])
+        s = 0.3
         ej14(x, y, s)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Resuleve el ejercicio 13 de la guia 8.')
+    parser = argparse.ArgumentParser(
+        description='Resuleve el ejercicio 13 de la guia 8.')
     parser.add_argument('items', metavar='I', type=str, nargs='+',
                         help='Los items a resolver.')
     args = parser.parse_args()
